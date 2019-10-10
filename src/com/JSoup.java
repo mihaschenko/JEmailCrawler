69f2c1e2dedff3146836feddcc58b810c48a0dd1
mihaschenko <mihaschenko.s@gmail.com>
2019-10-10 16:39:40 +0300

    new comments

--- src/com/JSoup.java
+++ src/com/JSoup.java
@@ -8,71 +8,79 @@ import java.io.IOException;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.ArrayList;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.net.URL;
 
 class JSoup
 {
 	public static void main(String[] args) throws IOException
 	{
-		System.out.println("START!");
-		
-		String strURL = "https://www.xing.com/companies";
-		URL mainURL = new URL(strURL);
+		String givenURL = "https://www.xing.com/companies";
+		String authority; // domain name
+		{
+			URL mainURL = new URL(givenURL);
+			authority = mainURL.getAuthority();
+		}
 		
 		String regex = "[a-zA-Z0-9\\.\\-\\_]+@[a-zA-Z]+[\\.]{1}[a-zA-Z]{2,4}";
 		Pattern pattern = Pattern.compile(regex);
 		
 		ArrayList<String> listOfURL = new ArrayList<String>();
 		ArrayList<String> listOfEmail = new ArrayList<String>();
-		ArrayList<String> emailSite = new ArrayList<String>();
-		listOfURL.add(mainURL.toString());
+		//ArrayList<String> emailAddresses = new ArrayList<String>();
+		listOfURL.add(givenURL);
 		
+		// main process/crawler loop
+		// основной процес
 		for(int i = 0; i < listOfURL.size() && i < 20; i++)
 		{
-			//Поиск url-ссылок и email-адресов на странице
-			strURL = listOfURL.get(i);
-				System.out.print("Подключение к " + strURL + " ");
+			givenURL = listOfURL.get(i);
+				System.out.print("Connect to " + givenURL + " ");
 				
-			Document doc = Jsoup.connect(strURL).get();
-			Elements scraped_urls = doc.select("a[href]");
+			Document doc = Jsoup.connect(givenURL).get();
+			Elements scrapedUrls = doc.select("a[href]");
 			
-			String text = doc.text();
-			Matcher matcher = pattern.matcher(text);
+			// поиск и сохранение емайл и их местонахождение
+			// search and save emails and their location
+			String siteText = doc.text();
+			Matcher matcher = pattern.matcher(siteText);
 			while(matcher.find())
 			{
 				if(!listOfEmail.contains(matcher.group()))
 				{
 					listOfEmail.add(matcher.group());
-					emailSite.add(strURL);
+					//emailAddresses.add(givenURL);
 				}
 			}
 			
-				//System.out.print(list.size() + " ");
-			
-			for(Element adr : scraped_urls)
+			// search and save URLs without duplication
+			// поиск и сохранение URL без дублирования
+			for(Element tag_a : scrapedUrls)
 			{
-				// Сохранение ссылок (без дублирования)
-				String str = adr.attr("abs:href");
+				String str = tag_a.attr("abs:href");
 				URL url = new URL(str);
-				if(mainURL.getAuthority().equals(url.getAuthority()))
+				if(authority.equals(url.getAuthority()))
 				{
 					if(!listOfURL.contains(str))
 						listOfURL.add(str);
 				}
 			}
-			//System.out.println(listOfURL.size());
+			System.out.print(" --- Found links (" + scrapedUrls.size() + ") ");
+			System.out.println("Saved links (" + listOfURL.size() + ") ");
 		}
-		System.out.println("Всего ссылок : " + listOfURL.size());
-		System.out.println("Всего почтовых адресов : " + listOfEmail.size());
-		
-		// Проверка массива на дублирования ссылок
-		Object[] ss = listOfURL.toArray();
-		Arrays.sort(ss);
-		for(int i = 0; i < ss.length && i < 50; i++)
-			System.out.println(ss[i]);
-		// Вывод найденных email-адресов
+		System.out.println("Total links : " + listOfURL.size());
+		System.out.println("Total Email Address : " + listOfEmail.size());
+
+		/* 
+		 * checking for duplicate links
+		Object[] checkList = listOfURL.toArray();
+		Arrays.sort(checkList);
+		for(int i = 0; i < checkList.length && i < 50; i++)
+			System.out.println(checkList[i]);
+			
+		 *  email output and their location
 		for(int i = 0; i < listOfEmail.size() && i < 50; i++)
-			System.out.println(listOfEmail.get(i) + " --- " + emailSite.get(i));
+			System.out.println(listOfEmail.get(i) + " --- " + emailAddresses.get(i));
+		*/
 	}
 }
